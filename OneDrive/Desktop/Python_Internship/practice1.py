29dfef77fef6b855a20451c1e1b7badeb868603e
Neha <mauryaneha2006@gmail.com>
2025-08-11 22:57:56 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/Python_Internship/practice1.py
+++
@@ -1,6 +0,0 @@
-import logging
-import logging.config
-
-logging.config.fileConfig('logfile.conf')
-logger = logging.getLogger('easyLogger')
-logger.debug('This is a debug log')
--- OneDrive/Desktop/Python_Internship/practice5.py
+++
@@ -1,9 +0,0 @@
-
-import logging
-import traceback
-
-try:
-    numbers = [10, 20, 30, 40, 50, 60]
-    chosen_number = numbers[10]
-except:
-    logging.error("Error: %s", traceback.format_exc())
